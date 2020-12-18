df22a1191dcad392722a9605a7cc9b01bfaada5d
kaiweinan <kaiweinan@yahoo.com>
2020-12-17 21:35:28 -0500

    commit tree a60de432b9dba1ac37f0c9425f8cfdbf9cdc0b43

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
-# Add  code here!
+
+def prime?(num)
+    return true if num == 2
+  return false if num <= 1 || num.even?
+  3.step(Math.sqrt(num).floor, 2) { |i| return false if (num % i).zero? }
+  true
+end
